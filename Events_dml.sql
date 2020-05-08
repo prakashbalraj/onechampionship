0ba2e7063d802c4d40edd742071b02c2fc6e0886
prakashbalraj <65019113+prakashbalraj@users.noreply.github.com>
2020-05-08 19:28:06 +0530

    Create Events_dml.sql
    Events Table Data

---
+++ Events_dml.sql
@@ -0,0 +1,15 @@
+insert all
+into events(id, event_name, people_count) values (1,'abc',100)
+into events(id, event_name, people_count) values (2,'abc',150)
+into events(id, event_name, people_count) values (3,'abc',170)
+into events(id, event_name, people_count) values (4,'def',50)
+into events(id, event_name, people_count) values (5,'def',110)
+into events(id, event_name, people_count) values (6,'def',130)
+into events(id, event_name, people_count) values (7,'xyz',120)
+into events(id, event_name, people_count) values (8,'def',170)
+into events(id, event_name, people_count) values (9,'xyz',150)
+into events(id, event_name, people_count) values (10,'xyz',110)
+into events(id, event_name, people_count) values (11,'abc',60)
+select * from dual;
+
+commit;
