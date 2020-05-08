72aed948415a546178fe75cc7b2343cb061c3553
prakashbalraj <65019113+prakashbalraj@users.noreply.github.com>
2020-05-08 19:56:34 +0530

    Update Events_DDL.sql

--- Events_DDL.sql
+++ Events_DDL.sql
@@ -1,7 +1,7 @@
 create table events
 (
-id int,
-event_name string,
-people_count int
+id int NOT NULL PRIMARY KEY,
+event_name varchar(100) NOT NULL,
+people_count int NOT NULL
 )comment 'Events Table'
 ;
